e39ad17db8527075ef2727d05146f5f3b9221228
TheEpicBlock <git.teb@theepicblock.nl>
2022-07-08 22:34:58 +0200

    Fix duping bug

---
+++ common/src/main/java/nl/theepicblock/shadowsgate/mixin/PatchRemoveOne.java
@@ -0,0 +1,36 @@
+package nl.theepicblock.shadowsgate.mixin;
+
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.collection.DefaultedList;
+import nl.theepicblock.shadowsgate.ShadowItem;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.List;
+
+@Mixin(PlayerInventory.class)
+public class PatchRemoveOne {
+    @Shadow @Final private List<DefaultedList<ItemStack>> combinedInventory;
+
+    @Shadow @Final public PlayerEntity player;
+
+    @Inject(method = "removeOne", at = @At("HEAD"))
+    public void removeFromInsideShadowItem(ItemStack itemStack, CallbackInfo ci) {
+        for (var inv : this.combinedInventory) {
+            for (var stack : inv) {
+                if (stack.getItem() instanceof ShadowItem) {
+                    var entry = ShadowItem.getOrCreateEntry(this.player.getWorld(), stack);
+                    if (entry.getStack() == itemStack) {
+                        entry.setStack(ItemStack.EMPTY);
+                    }
+                }
+            }
+        }
+    }
+}
